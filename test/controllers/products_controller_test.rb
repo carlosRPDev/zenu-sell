3583d76ddde7ea025e481a018690f0b8da24558f
Carlos Rodriguez <ucroxlive@gmail.com>
2022-09-21 13:27:23 -0500

    Modificamos test que valida el renderizado de los productos, agregamos test para validar que al dar click en una categoria para filtrar muestre los cantidad de producto

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -6,6 +6,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select '.product', 3
+    assert_select '.category', 4
+  end
+
+  test 'render a list of products filtered by category' do
+    get products_path(category_id: categories(:computers).id)
+
+    assert_response :success
+    assert_select '.product', 1
+    assert_select '.category', 4
   end
 
   test 'render a detailed product page' do
