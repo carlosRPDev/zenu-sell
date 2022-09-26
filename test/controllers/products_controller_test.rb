b98b7df1425e7c1608dfbfd473fb7520fdcff6b7
Carlos Rodriguez <ucroxlive@gmail.com>
2022-09-26 02:31:58 -0500

    refatoriza codigo de los test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,23 +5,23 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     get products_path
 
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.category', 4
+    assert_select '.product', 12
+    assert_select '.category', 10
   end
 
   test 'render a list of products filtered by category' do
     get products_path(category_id: categories(:computers).id)
 
     assert_response :success
-    assert_select '.product', 1
-    assert_select '.category', 4
+    assert_select '.product', 5
+    assert_select '.category', 10
   end
 
   test 'render a list of products filtered by min_price and max_price' do
     get products_path(min_price: 100000, max_price: 210000)
 
     assert_response :success
-    assert_select '.product', 2
+    assert_select '.product', 8
     assert_select 'h2', 'Tv Samsung 43 pulgadas'
     assert_select 'h2', 'Teclado Mecanico Keychrom K2 v2'
   end
@@ -38,16 +38,16 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     get products_path(order_by: 'expensive')
 
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.products .product:first-child h2', 'PS4 Fat'
+    assert_select '.product', 12
+    assert_select '.products .product:first-child h2', 'Seat Panda clásico'
   end
 
   test 'sort products by cheapest price first' do
     get products_path(order_by: 'cheapest')
 
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.products .product:first-child h2', 'Tv Samsung 43 pulgadas'
+    assert_select '.product', 12
+    assert_select '.products .product:first-child h2', 'El hobbit'
   end
 
   test 'render a detailed product page' do
