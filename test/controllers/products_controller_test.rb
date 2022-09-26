c7292e5ce81cd783b4f2ef51761719ab59a93edb
Carlos Rodriguez <ucroxlive@gmail.com>
2022-09-26 01:19:42 -0500

    Agregamos los test para verificar que se este renderizando el producto mas barato, mas caro y el mas reciente

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -34,6 +34,22 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h2', 'PS4 Fat'
   end
 
+  test 'sort products by expensive price first' do
+    get products_path(order_by: 'expensive')
+
+    assert_response :success
+    assert_select '.product', 3
+    assert_select '.products .product:first-child h2', 'PS4 Fat'
+  end
+
+  test 'sort products by cheapest price first' do
+    get products_path(order_by: 'cheapest')
+
+    assert_response :success
+    assert_select '.product', 3
+    assert_select '.products .product:first-child h2', 'Tv Samsung 43 pulgadas'
+  end
+
   test 'render a detailed product page' do
     get product_path(products(:TecladoMecanico))
 
