43c4d3a335a58c76dfc0e12789e75c1ccde64c67
Carlos Rodriguez <ucroxlive@gmail.com>
2022-10-08 13:26:41 -0500

    Agrega test para eliminar un favorito

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -3,14 +3,23 @@ require "test_helper"
 class FavoritesControllerTest < ActionDispatch::IntegrationTest
   setup do
     login
-    @product = products(:tv)
+    @tv = products(:tv)
+    @Teclado_mecanico = products(:TecladoMecanico)
   end
 
   test "should create favorite" do
     assert_difference('Favorite.count', 1) do
-      post favorites_url(product_id: @product.id)
+      post favorites_url(product_id: @tv.id)
     end
     
-    assert_redirected_to product_path(@product)
+    assert_redirected_to product_path(@tv)
+  end
+
+  test "should unfavorite" do
+    assert_difference('Favorite.count', -1) do
+      delete favorite_url(@Teclado_mecanico.id)
+    end
+    
+    assert_redirected_to product_path(@Teclado_mecanico)
   end
 end
